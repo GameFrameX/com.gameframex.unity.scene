b2aa9d4af8509192855c802dca2a09911dfad366
AlianBlank <wangfj11@foxmail.com>
2024-04-10 13:03:50 +0800

    [增加]1. 增加场景组件

---
+++ Runtime/GameFrameXSceneCroppingHelper.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace GameFrameX.Scene.Runtime
+{
+    public class GameFrameXSceneCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(SceneComponent);
+            _ = typeof(ActiveSceneChangedEventArgs);
+            _ = typeof(GameSceneManager);
+            _ = typeof(IGameSceneManager);
+            _ = typeof(LoadSceneFailureEventArgs);
+            _ = typeof(LoadSceneSuccessEventArgs);
+            _ = typeof(LoadSceneUpdateEventArgs);
+            _ = typeof(UnloadSceneFailureEventArgs);
+            _ = typeof(UnloadSceneSuccessEventArgs);
+        }
+    }
+}
